bbe901cbc5ebc395c70f4b590e683efb5ca2a53a
Pea Tyczynska <CrystalPea@users.noreply.github.com>
2021-12-30 17:27:04 +0000

    Show a single page and refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root "pages#index"
 
-  get "/pages", to: "pages#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :pages
 end
